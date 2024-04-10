7e7396c189f3ada7d43de5219c61e54b3dee0278
J5Casey <149739665+J5Casey@users.noreply.github.com>
2024-04-10 13:45:24 -0500

    Changed from scanner to data input stream

--- src/main/java/TradeStock.java
+++ src/main/java/TradeStock.java
@@ -1,14 +1,23 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class TradeStock {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
         if(args.length != 2){
             System.out.println("Incorrect number of arguments. Please run as java -jar TradeStock.jar {binary file} {algorithm to use}\n");
             System.out.println("The options for algorithms (not case sensitive) are: DivCon_nlogn, DivCon_linear, DecCon_linear");
-        } else {
-            Scanner scan = new Scanner(new File(args[0]));
+            return;
+        }
+
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(args[0]))) {
+            int n = dis.readInt();
+            float[] prices = new float[n];
+            for (int i = 0; i < n; i++) {
+                prices[i] = dis.readFloat();
+            }
+    
+
             String algorithm = args[1].toLowerCase();
             switch (algorithm){
                 case "divcon_nlogn":
@@ -23,7 +32,14 @@ public class TradeStock {
                 default:
                     System.out.println("Invalid algorithm specified. Please choose one of the following: DivCon_nlogn, DivCon_linear, DecCon_linear");
             }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the binary file: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
         }
+        
     }
 
     public static void DivCon_NlogN(){
